7f5bae53f7fa9a1bd28e23b1abf8ca97131fb368
StativaCamelia <48285471+StativaCamelia@users.noreply.github.com>
2019-12-20 23:25:32 +0200

    Add files via upload

---
+++ client_new.c
@@ -0,0 +1,180 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <signal.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/select.h>
+
+#define MSGSIZE 500
+#define PORT 2019
+/* codul de eroare returnat de anumite apeluri */
+extern int errno;
+
+/* portul de conectare la server*/
+int port;
+
+//variabila care anunta inchiderea
+int cancel = 0;
+int logat = 0;
+
+fd_set master;
+fd_set read_fds;
+
+//FUCTII
+void print_meniu();
+
+void *receive_function(void *arg);
+void *speed_update(void *arg);
+void *commands_send(void *arg);
+
+void send_function(char *msg_de_trimis);
+
+void login_meniu();
+void main_meniu();
+
+//mutex
+pthread_mutex_t lock =PTHREAD_MUTEX_INITIALIZER;
+
+//mesaje
+char msg_trimis[MSGSIZE];
+
+//sock
+int sock_d;
+
+int main(int argc, char* argv[])
+{       
+        /*
+        FD_ZERO(&master);
+        FD_ZERO(&read_fds);
+
+        FD_SET(0,&master);
+        FD_SET(sock_d,&master);
+        
+        if (select(sock_d+1, &read_fds, NULL,NULL,NULL) == -1)
+        {
+            perror("[server]Select");
+        }
+        */
+        //structura server
+        struct sockaddr_in server;
+        /* cream socketul */
+        if ((sock_d = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror ("Eroare la socket().\n");
+            return errno;
+        }
+         // umplem structura folosita pentru realizarea conexiunii cu serverul 
+         //familia socket-ului 
+         server.sin_family = AF_INET;
+        // adresa IP a serverului 
+        server.sin_addr.s_addr = inet_addr("127.0.0.1");
+        // portul de conectare 
+        server.sin_port = htons (PORT);
+        // ne conectam la server 
+       
+        if (connect (sock_d, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
+        {
+            perror ("[client]Eroare la connect().\n");
+            return errno;
+        }
+   
+        print_meniu();
+
+        pthread_t speed_thread, recv_thread, command_thread;
+        int thread_1, thread_2;
+
+        if(pthread_mutex_init(&lock, NULL))
+        {
+            printf("Initializarea mutex nu a reusit");
+        }
+
+        pthread_create(&speed_thread, NULL, &speed_update, NULL);
+        pthread_create(&command_thread, NULL, &commands_send, NULL);
+        pthread_join(command_thread, NULL);
+        pthread_join(speed_thread, NULL);
+        
+        pthread_mutex_destroy(&lock);
+        exit(1);
+}
+
+void send_function(char *msg_de_trimis)
+{
+    int lungime_int;
+    char lungime_str[3];
+    pthread_mutex_lock(&lock);
+    lungime_int = strlen(msg_de_trimis);
+    sprintf(lungime_str,"%d", lungime_int-1);
+    if(write(sock_d, lungime_str, sizeof(lungime_str)) <= 0)
+    {
+        perror("[client] Mesajul cu lungimea NU a fost trimis");
+        return errno;
+    }
+    if(write(sock_d, msg_de_trimis, lungime_int) <= 0)
+    {
+        perror("[client] Mesajul cu date NU a fost trimis");
+        return errno;
+    }
+    printf("%s", msg_de_trimis);
+    pthread_mutex_unlock(&lock);
+}
+
+void *speed_update(void *arg)
+{
+    int viteza_int;
+    char viteza_str[3];
+    if(logat == 0)
+    {
+        while(1)
+        {   fflush(stdin);
+            fflush(stdout);
+            srand(time(NULL));
+            viteza_int = 80;
+            sprintf(viteza_str, "%d", viteza_int);
+            send_function(viteza_str);
+            sleep(60);
+        }
+    }
+}
+
+void *commands_send(void *arg)
+{
+    char commanda[MSGSIZE];
+    while(1)
+    {
+        bzero(&commanda,sizeof(msg_trimis));
+        fflush(stdin);
+        fflush(stdout);
+        read(0, commanda, sizeof(commanda));
+        send_function(commanda);
+    }
+}
+void print_meniu()
+{
+     //---------------->MENIU<------------------
+    printf("---------------------------------------------->\n");
+    printf("SALUT!\n");
+    printf("Select an option:\n");
+    printf("Login\n");
+    printf("Sign in\n");
+    printf("Help\n");
+    printf("Ouit\n");
+    printf("---------------------------------------------->\n");
+}
+
+void main_meniu()
+{
+    printf("---------------------------------------------->\n");
+    printf("Sunteti conectat ca si Utilizator1\n");
+    printf("Trimite un eveniment din trafic (Trafic info)\n");
+    printf("Deconectare\n");
+    printf("Abonare la news(Update Settings)\n");
+    printf("Quit\n");
+    printf("----------------------------------------------->\n");
+
+}
---
+++ server.c
@@ -0,0 +1,249 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sqlite3.h>
+
+#define PORT 2019 //PORTUL FOLOSIT
+#define MSGSIZE 500
+#define MAXCLIENTS 100
+//baza de date
+sqlite3* db;
+
+//structura folosita de clienti
+static int uid = 10;
+
+typedef struct
+{
+int sock_id;
+char ip[INET_ADDRSTRLEN];
+int UID;
+} client_info;
+
+//lista de clienti
+client_info *clients[MAXCLIENTS];
+
+//functii:
+void add_client(client_info * cl);
+void delete_client(int sock_id);
+void send_message_to_all(char* message);
+void print_clients();
+
+void send_msg(char *msg_trimis);
+void recv_msg(void *argc);
+
+extern int errno;
+
+void sigchld_handler(int s)
+{
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+int main(int argc, char*argv[])
+{
+
+
+    //Ce comenzi poate sa primeasca serverul:
+    char login[] = "Login";
+    char sign_in[] = "Sign in";
+    char traffic_info[] = "Trafic info";
+    char update_settings[] = "Update Settings";
+    char update_location[] = "Update Location";
+    char update_speed[] = "Update Speed"; 
+    char quit[] = "QUIT";
+    char help[] = "Help";
+
+    //structura folosita de server
+    struct sockaddr_in server;
+
+    //structura pentru clienti 
+    struct sockaddr_in from;
+
+    //descriptor socket server
+    int sock_serv;
+
+    //descriptor socket client
+    int sock_client;
+
+    //creare socket
+    if((sock_serv = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("[server] Eroare la creare socket\n");
+            return errno;
+        }
+    int on=1;
+    setsockopt(sock_serv,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
+    //pregatire structuri date
+    bzero(&server, sizeof(server));
+    bzero(&from, sizeof(from));
+
+    //pregatim structura server1
+    //familie socket
+    server.sin_family = AF_INET;
+    //acceptam orice adresa
+    server.sin_addr.s_addr = htonl(INADDR_ANY); 
+    //setam portul
+    server.sin_port = htons(PORT);
+
+    
+    //atasam socket
+    if (bind (sock_serv, (struct sockaddr *) &server, sizeof (struct sockaddr)) == -1)
+    {
+      perror ("[server]Eroare la bind().\n");
+      return errno;
+    }
+
+    //putem serverul sa asculte
+    if(listen(sock_serv,10) == -1)
+    {
+        perror("[server] Eroare la listen\n");
+        return errno;
+    }
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("[server] Astept clientii la portul: %d\n",PORT);
+   
+    //ip-ul clientilor
+    char ip[INET_ADDRSTRLEN];
+   
+    //incepem sa servim in mod concurent clientii
+    while(1)
+    {
+        fflush(stdout);
+   
+        //acceptam un client
+        int lenght = sizeof(from);
+        sock_client = accept(sock_serv, (struct sockaddr *)&from, &lenght);
+        if(sock_client < 0)
+        {
+            perror("[server] Eroare la accept\n");
+            continue;
+        }
+        //adaugam clientul la lista actuala de clienti
+        client_info *cl = (client_info*)malloc(sizeof(client_info));
+   
+        //client uid and socket
+        dup2(sock_client,uid);
+        cl->sock_id = uid;
+        cl->UID = uid;
+        uid++;
+        //convertim addresa clientului intr-un string
+        inet_ntop(AF_INET, (struct sockaddt*)&from, ip, INET_ADDRSTRLEN);
+        //adaugam ip-ul clientului
+        strcpy(cl->ip,ip);
+        
+        //adaugam socketul clientului
+        cl->sock_id = sock_client;
+        add_client(cl);
+   
+        int pid = fork();
+        if(pid == 0)
+        {   
+            fflush(stdout);
+            fflush(stdin);
+
+            close(sock_serv);
+            int ok_logat = 0;
+            char locatie[3];
+            char viteza[3];
+            int cancel = 0;
+
+            bzero(&locatie,sizeof(locatie));
+            bzero(&viteza, sizeof(viteza));
+
+            char lungime_str[3];
+            int lungime_int = 0;
+            int stop;
+            while(1)
+            {
+                fflush(stdout);
+                fflush(stdin);
+
+                bzero(lungime_str, sizeof(lungime_str));
+        
+                if((stop=read(uid-1, lungime_str, sizeof(lungime_str)))<0)
+                {
+                    perror("[server] Eroare la citirea lungimii in server");
+                    return errno;
+                }
+                if(stop == 0)
+                    break;
+                lungime_int = atoi(lungime_str);
+                char *msg_primit = (char*)malloc(lungime_int);
+                
+                if(read(uid-1, msg_primit, lungime_int+1)<0)
+                {
+                    perror("[server] Eroare la citirea mesajului in server");
+                    return errno;
+                } 
+                if(stop == 0)
+                    break;
+                printf("Mesaj primit:%s\n",msg_primit);
+            }//end while(1)
+            exit(1);
+        }//end if fork
+        close(sock_client);
+        close(uid);
+    }
+}
+    //adaug un client la lista actuala de clienti conectati la server
+    void add_client(client_info * cl)
+    {   
+        for(int i = 0; i< MAXCLIENTS; ++i)
+        {   
+            if(!clients[i])
+            {  
+                clients[i] = cl;
+                break;
+            }
+        }
+    }
+    //elimin un client din lista actuala de clienti conectati la server
+    void delete_client(int sock_id)
+    {
+        for(int i=0; i < MAXCLIENTS; i++)
+        {
+            if(clients[i])
+            {
+                if(clients[i]->sock_id == sock_id)
+                {
+                    clients[i] = NULL;
+                    break;
+                }
+            }
+        }
+    }
+
+void send_message_to_all(char* message)
+{   
+    char *send = (char*) malloc((strlen(message)+5)*sizeof(char));
+    strcpy(send,"INFO:");
+    strcat(send,message);
+    for(int i=0; i<MAXCLIENTS;i++)
+    {   
+        if(clients[i])
+        {   
+            if(write(clients[i]->UID, send, strlen(send)) < 0)
+            {
+                perror("[server] Eroare la scriere in send_to_all");
+                break;
+            }
+        }
+    }    
+}
